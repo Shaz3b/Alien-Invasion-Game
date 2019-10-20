6c8cef468c23f20a408b1c107e87f6d02157235a
Shaz3b <Shazebhussaini1994@gmail.com>
2019-10-20 19:31:09 +0530

    added ship image

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,13 +2,14 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
   pygame.init()
   ai_settings = Settings()
   screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
   pygame.display.set_caption("Alien Invastion")
-
+  ship = Ship(screen)
 
   while True:
     for i in pygame.event.get():
@@ -16,6 +17,7 @@ def run_game():
         sys.exit()
 
     screen.fill(ai_settings.bg_color)
+    ship.blitme()
     pygame.display.flip()
 
 run_game()
