9e3336e42491868cf5cc1617fe6523acc084ee47
Shaz3b <Shazebhussaini1994@gmail.com>
2019-10-20 16:53:51 +0530

    install update

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash -e
 
 if [[ $(command brew -v) == "" ]]; then
-  echo "Installing brew OS X."
+  echo "Installing brew on OS X."
   (/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)")
 else
   echo "Updating brew"
   brew update
 fi
   
+if [[ $(command pip3 -v) == "" ]]; then
+  echo "Installing pip3 on OS X."
+  (python3 get-pip.py)
+else
+  echo "You have pip3"
+fi
 
+echo "Install requirement libraries."
+(pip3 install -r requirements.txt)
