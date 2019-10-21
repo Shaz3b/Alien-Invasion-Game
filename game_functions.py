9cd11e974c8022b1492ebaba46353803dc009cde
Shaz3b <Shazebhussaini1994@gmail.com>
2019-10-21 17:54:36 +0530

    Blue

--- game_functions.py
+++ game_functions.py
@@ -5,3 +5,9 @@ def check_events():
   for i in pygame.event.get():
     if i.type == pygame.QUIT:
       sys.exit()
+ 
+def update_screen(ai_settings, screen, ship):
+  screen.fill(ai_settings.bg_color)
+  ship.blitme()
+
+  pygame.display.flip()
